e800b57ca1e2b698c5a50e3207b074cdc40b40cd
nrs23 <nrs23@sussex.ac.uk>
2023-11-09 14:14:08 +0000

    closes #2, closes #4, closes #6; [FEATURE] Deploy rust without docker

---
+++ deploy-rust.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+./stop-api.sh
+
+if ! [ -d "$HOME/waterlogged-prototype/dockerised-rust-api/" ]; then
+    echo 'docker app directory does not exist!'
+    exit 1
+fi
+
+cd $HOME/waterlogged-prototype/dockerised-rust-api/
+cargo build --release
+if [ $? != 0 ]; then
+    echo "cargo  build failed, check the server logs!"
+    exit 1
+fi
+
+./target/release/api-example &
+
+# Capture PID of last executed command with `$!`
+# and use it to generate a kill script
+echo "kill $!" > ../stop-api.sh
+
+chmod +x ../stop-api.sh
